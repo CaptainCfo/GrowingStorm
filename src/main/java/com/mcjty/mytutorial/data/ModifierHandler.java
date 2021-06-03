621fede5be38957156719278d4ff997ca41ad3a7
CaptainCfo <colton200088@yahoo.com>
2021-06-02 22:43:03 -0400

    init

--- src/main/java/com/mcjty/mytutorial/data/ModifierHandler.java
+++ src/main/java/com/mcjty/mytutorial/data/ModifierHandler.java
@@ -2,6 +2,7 @@ package com.mcjty.mytutorial.data;
 
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.entity.monster.EndermanEntity;
 import net.minecraft.entity.monster.SkeletonEntity;
 import net.minecraft.entity.monster.SpiderEntity;
@@ -19,37 +20,35 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class ModifierHandler {
     @SubscribeEvent
     public static void onMobSlay(LivingDeathEvent event){
-        DamageSource source = event.getSource();
-        if(!(source.getEntity() instanceof PlayerEntity)){
+        if(!(event.getSource().getEntity() instanceof PlayerEntity)){
             return;
         }
         // DataParameter<EntityHealth> health = new DataParameter<EntityHealth>(1, );
-
         if(event.getEntity() instanceof ZombieEntity ){
-            ZombieEntity.createLivingAttributes()
+            ZombieEntity.createMobAttributes()
                     .add(Attributes.ATTACK_DAMAGE, (Attributes.ATTACK_DAMAGE.getDefaultValue() * 100D))
                     .add(Attributes.MAX_HEALTH, 100D);
         }
         if(event.getEntity() instanceof SkeletonEntity){
-            SkeletonEntity.createLivingAttributes()
+            SkeletonEntity.createMobAttributes()
                     .add(Attributes.ATTACK_DAMAGE, (Attributes.ATTACK_DAMAGE.getDefaultValue() * .01D))
                     .add(Attributes.MAX_HEALTH, (Attributes.MAX_HEALTH.getDefaultValue() * .01D))
                     .add(Attributes.ARMOR, (Attributes.ARMOR.getDefaultValue() * .01D));
         }
         if(event.getEntity() instanceof SpiderEntity){
-            SpiderEntity.createLivingAttributes()
+            SpiderEntity.createMobAttributes()
                     .add(Attributes.ATTACK_SPEED, (Attributes.ATTACK_SPEED.getDefaultValue() * .01D))
                     .add(Attributes.KNOCKBACK_RESISTANCE, (Attributes.KNOCKBACK_RESISTANCE.getDefaultValue() * .01D))
                     .add(Attributes.MAX_HEALTH, (Attributes.MAX_HEALTH.getDefaultValue() * .01D));
         }
         if(event.getEntity() instanceof PiglinEntity){
-            PiglinEntity.createLivingAttributes()
+            PiglinEntity.createMobAttributes()
                     .add(Attributes.MOVEMENT_SPEED, (Attributes.MOVEMENT_SPEED.getDefaultValue() * .01D))
                     .add(Attributes.ATTACK_DAMAGE, (Attributes.ATTACK_DAMAGE.getDefaultValue() * .01D))
                     .add(Attributes.MAX_HEALTH, (Attributes.MAX_HEALTH.getDefaultValue() * .01D));
         }
         if(event.getEntity() instanceof EndermanEntity){
-            EndermanEntity.createLivingAttributes()
+            EndermanEntity.createMobAttributes()
                     .add(Attributes.MOVEMENT_SPEED, (Attributes.MOVEMENT_SPEED.getDefaultValue() * .01D))
                     .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, (Attributes.SPAWN_REINFORCEMENTS_CHANCE.getDefaultValue() * .04D))
                     .add(Attributes.MAX_HEALTH, (Attributes.MAX_HEALTH.getDefaultValue() * .01D));
